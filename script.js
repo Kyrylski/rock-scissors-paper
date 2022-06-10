a5b41bd9017e634681642a0d8b7d78a4784dadc3
Kyrylski <kyryldubovyi@gmail.com>
2022-06-10 10:50:25 +0200

    Update choises and loop for round

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-// prompt("Rock Scissors Paper")
+
 let randomChoise;
 let computerChoise;
-let playerSelection;
+let playerChoise;
 let computerSelection;
-let gameRound;
+let result;
+
+
 
 let computerPlay = function () {
     
@@ -16,10 +18,35 @@ let computerPlay = function () {
       }else (randomChoise === 3); {
         computerChoise = "Paper"
       }
-console.log(computerChoise)
+return computerChoise
 
   }
 
+  let gameRound = function(playerChoise,computerChoise){
+    playerChoise = prompt("Rock Scissors Paper");
+    if (playerChoise === computerChoise) {
+        result = "it's a draw"
+    }else if (playerChoise === "Rock" && computerChoise === "Scissors"){
+        result = "You win!"
+    }else if (playerChoise === "Rock" && computerChoise === "Paper"){
+        result = "You lose!"
+    }else if (playerChoise === "Scissors" && computerChoise === "Paper"){
+        result = "You win!"
+    }else if (playerChoise === "Scissors" && computerChoise === "Rock"){
+        result = "You lose!"
+    }else if (playerChoise === "Paper" && computerChoise === "Rock"){
+        result = "You win!"
+    }else if(playerChoise === "Paper" && computerChoise === "Scissors"){
+        result = "You lose!"
+    }
 
 
-  
+
+  }
+
+let game = function() {
+    for(i = 0; i < 5; i++){
+        gameRound()
+        console.log(result)
+    }
+}  
