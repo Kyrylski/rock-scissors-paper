5825a86408b5a8a1ae1f88df62594d6bf3e8f71b
Kyrylski <kyryldubovyi@gmail.com>
2022-06-16 17:03:40 +0200

    New solution, player and computer choices

--- script.js
+++ script.js
@@ -1,52 +1,82 @@
 
-let randomChoise;
-let computerChoise;
-let playerChoise;
-let computerSelection;
-let result;
 
 
+// User choosing rock scissors or paper
 
-let computerPlay = function () {
+
+let playerChoice = prompt("What's your move");
+
+
+// computer choosing rock scissors or paper
+
+function choiceGenerator() {
     
-    let randomChoise = Math.floor(Math.random() * 3) + 1;
+       randomChoice = Math.floor(Math.random() * 3) + 1;
+    
+        if(randomChoice === 1) {
+            return  "Rock";
+          }else if (randomChoice === 2) {
+            return  "Scissors";
+          }else (randomChoice === 3); {
+            return  "Paper";
+          }
+  
+        }
 
-    if(randomChoise === 1) {
-        computerChoise = "Rock"
-      }else if (randomChoise === 2) {
-        computerChoise = "Scissors"
-      }else (randomChoise === 3); {
-        computerChoise = "Paper"
-      }
-return computerChoise
-
-  }
-
-  let gameRound = function(playerChoise,computerChoise){
-    playerChoise = prompt("Rock Scissors Paper");
-    if (playerChoise === computerChoise) {
-        result = "it's a draw"
-    }else if (playerChoise === "Rock" && computerChoise === "Scissors"){
-        result = "You win!"
-    }else if (playerChoise === "Rock" && computerChoise === "Paper"){
-        result = "You lose!"
-    }else if (playerChoise === "Scissors" && computerChoise === "Paper"){
-        result = "You win!"
-    }else if (playerChoise === "Scissors" && computerChoise === "Rock"){
-        result = "You lose!"
-    }else if (playerChoise === "Paper" && computerChoise === "Rock"){
-        result = "You win!"
-    }else if(playerChoise === "Paper" && computerChoise === "Scissors"){
-        result = "You lose!"
-    }
+        let computerChoice = choiceGenerator()
 
 
 
-  }
 
-let game = function() {
-    for(i = 0; i < 5; i++){
-        gameRound()
-        console.log(result)
-    }
-}  
+
+
+
+
+
+
+
+
+// playerChoise = prompt("Wybierz coś")
+
+// let computerPlay = function () {
+    
+//     let randomChoise = Math.floor(Math.random() * 3) + 1;
+
+//     if(randomChoise === 1) {
+//         computerChoise = "Rock"
+//       }else if (randomChoise === 2) {
+//         computerChoise = "Scissors"
+//       }else (randomChoise === 3); {
+//         computerChoise = "Paper"
+//       }
+// return computerChoise
+
+//   }
+
+//   let gameRound = function(playerChoise,computerChoise){
+//     playerChoise = prompt("Rock Scissors Paper");
+//     if (playerChoise === computerChoise) {
+//         result = "it's a draw"
+//     }else if (playerChoise === "Rock" && computerChoise === "Scissors"){
+//         result = "You win!"
+//     }else if (playerChoise === "Rock" && computerChoise === "Paper"){
+//         result = "You lose!"
+//     }else if (playerChoise === "Scissors" && computerChoise === "Paper"){
+//         result = "You win!"
+//     }else if (playerChoise === "Scissors" && computerChoise === "Rock"){
+//         result = "You lose!"
+//     }else if (playerChoise === "Paper" && computerChoise === "Rock"){
+//         result = "You win!"
+//     }else if(playerChoise === "Paper" && computerChoise === "Scissors"){
+//         result = "You lose!"
+//     }
+
+
+
+//   }
+
+// let game = function() {
+//     for(i = 0; i < 5; i++){
+//         gameRound()
+//         console.log(result)
+//     }
